979337a5f3810622297e1d1118f8103fce66fbb5
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-17 22:54:18 +0100

    Create TestArrayPractice.java

---
+++ Java project 1/Activity 1/src/TestArrayPractice.java
@@ -0,0 +1,30 @@
+
+/**
+ * A simple tester class to test the ArrayPractice class methods
+ * @author A. Razak
+ * version 17/02/2019
+ */
+
+
+public class TestArrayPractice {
+
+	
+	public static void main(String args[]){
+		
+	ArrayPractice ap = new ArrayPractice ();
+	
+	// Exercise 1
+	ap.PrintArray();
+	ap.smallestNumber();
+	ap.largestNumber();
+	ap.PrintFibonacci();
+	ap.markAnalysisForEach();
+	ap.markAnalysisWhile();
+	ap.markAnalysisFor();
+	
+	ap.PrintArray();
+	
+	
+	}
+	
+}
