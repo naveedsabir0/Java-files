6062de6b48e284024cb2031a43d81275059ecb3d
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-17 22:54:52 +0100

    Create Person.java

---
+++ Java project 1/Activity 2/Person.java
@@ -0,0 +1,80 @@
+
+/**
+ * Simple class that records the name and age of a person
+ * 
+ * @author Abdul Razak 
+ * @version 1.0
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private int age;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person()
+    {
+        // initialise instance variables
+        name = "Nick";
+        age = 22;
+    }
+
+    /**
+     * Parameter constructor for Person
+     */
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    
+    /**
+     * Set the person's name
+     * 
+     * @param  name   the new name to use
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    /**
+     * Get the person's name
+     * 
+     * @return the person's name 
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * Set the person's age
+     * 
+     * @param  age  the person's age
+     */
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+    
+    /**
+     * Get the person's age
+     * 
+     * @return the person's age 
+     */
+    public int getAge()
+    {
+        return age;
+    }
+    
+    /**
+     * The string to use when printing out
+     */
+    public String toString()
+    {
+        return name + "  (" + age + " years old)";
+    }
+}
