102edc0416f10e28d90756ca0ca51134a755e45b
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-17 23:58:38 +0100

    Create PersonTester.java

---
+++ Java project 7/person/src/PersonTester.java
@@ -0,0 +1,14 @@
+package person;
+
+public class PersonTester {
+	
+	public static void main (String[] args)
+	 {
+		 Person p1;
+		 p1= new Person (21,"Sami");
+		 p1.setAge(21);
+		 p1.setName("Sam");
+		 p1.printDetails();
+	 }
+
+}
