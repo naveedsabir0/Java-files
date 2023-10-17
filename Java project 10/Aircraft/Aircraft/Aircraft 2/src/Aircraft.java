400e5a0fc8ca51f4b790c424877de20e3a22ed5f
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-18 00:17:26 +0100

    Create Aircraft.java

---
+++ Java project 10/Aircraft/Aircraft/Aircraft 2/src/Aircraft.java
@@ -0,0 +1,71 @@
+//package ar5;
+
+/**
+ * Write a description of class Aircraft here.
+ * 
+ * @author Abdul Razak
+ * @version 1.0
+ */
+public class Aircraft 
+{
+	protected int altitude = 0;
+	protected int speed = 0;
+	
+	/**
+	 * Set the aircraft stationary on the ground
+	 */
+	public Aircraft() 
+	{
+	}
+
+	/**
+	 * Make sure the aircraft is above ground and not
+	 * going backwards.
+	 * 
+	 * @param altitude
+	 * @param speed
+	 */
+	public Aircraft(int altitude, int speed) 
+	{
+		setAltitude(altitude);
+		setSpeed(speed);
+	}
+
+	/**
+	 * @return the altitude
+	 */
+	public int getAltitude() 
+	{
+		return altitude;
+	}
+
+	/**
+	 * Make sure the aircraft at or above sea level
+	 * @param altitude the altitude to set
+	 */
+	public void setAltitude(int altitude) 
+	{
+		if(altitude >= 0){
+			this.altitude = altitude;
+		}
+	}
+
+	/**
+	 * Don't let the aircraft fly backwards
+	 * @return the speed
+	 */
+	public int getSpeed() 
+	{
+		return speed;
+	}
+
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) 
+	{
+		if(speed >= 0){
+			this.speed = speed;
+		}
+	}
+}
