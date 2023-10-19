eaf78873efa268fbcdc75f2da53b64391310d778
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-19 13:38:20 +0100

    Create Payable.java

---
+++ Java project 11/interfaceExercise/InterfaceExercise/src/Payable.java
@@ -0,0 +1,14 @@
+
+// Defining an interface: Payable.java
+// Payable interface declaration.
+/**
+ * 
+ * @author A.Razak
+ *
+ */
+
+public interface Payable 
+{    
+   double getPaymentAmount(); // calculate payment; no implementation
+   public void printDetails();// Method to display details; no implementation
+} // end interface Payable
