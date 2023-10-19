fe2ba29336218a9de8365ff26b8bf810a9c7ef4c
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-19 13:38:58 +0100

    Create Shape.java

---
+++ Java project 11/JavaAbstractProject/src/Shape.java
@@ -0,0 +1,14 @@
+package defaultpackage;
+
+public abstract class Shape {
+public String color;
+public Shape() {
+}
+public void setColor(String c) {
+color = c;
+}
+public String getColor() {
+return color;
+}
+abstract public double area();
+}
