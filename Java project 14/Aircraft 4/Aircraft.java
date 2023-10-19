46c3f338257bc2f4da474a4aeba4d9419df060b8
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-19 13:44:28 +0100

    Create Aircraft.java

---
+++ Java project 14/Aircraft 4/Aircraft.java
@@ -0,0 +1,79 @@
+/**
+ * Write a description of the abstract class 
+ * Aircraft here.
+ * 
+ * @author Abdul Razak
+ * @version 1.0
+ */
+public class Aircraft 
+{
+    protected int altitude = 0;
+    protected int speed = 0;
+
+    /**
+     * Set the aircraft stationary on the ground
+     */
+    public Aircraft() 
+    {
+    }
+    
+    /**
+    * This method has been added to enable polymorphism.
+    */
+    public void useWeapons(){
+     System.out.println("I have no weapons");
+    }
+
+
+    /**
+     * Make sure the aircraft is above ground and not
+     * going backwards.
+     * 
+     * @param altitude
+     * @param speed
+     */
+    public Aircraft(int altitude, int speed) 
+    {
+        setAltitude(altitude);
+        setSpeed(speed);
+    }
+  
+    /**
+     * @return the altitude
+     */
+    public int getAltitude() 
+    {
+        return altitude;
+    }
+
+    /**
+     * Make sure the aircraft at or above sea level
+     * @param altitude the altitude to set
+     */
+    public void setAltitude(int altitude) 
+    {
+        if(altitude >= 0){
+            this.altitude = altitude;
+        }
+    }
+
+    /**
+     * @return the speed
+     */
+    public int getSpeed() 
+    {
+        return speed;
+    }
+
+    /**
+     * Don't let the aircraft fly backwards
+     * @param speed the speed to set
+     */
+    public void setSpeed(int speed) 
+    {
+        if(speed >= 0){
+            this.speed = speed;
+        }
+    }
+
+}
