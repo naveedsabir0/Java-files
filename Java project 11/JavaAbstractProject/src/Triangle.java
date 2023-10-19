176247279cb48ccac94311be954f4e7ca66a7489
naveedsabir0 <sabirnaveed2@gmail.com>
2023-10-19 13:39:02 +0100

    Create Triangle.java

---
+++ Java project 11/JavaAbstractProject/src/Triangle.java
@@ -0,0 +1,16 @@
+package defaultpackage;
+
+public class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
+
